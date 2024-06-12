107c51b35d8156f6edf5bedf9cbc10128640a82d
Jogender866 <jogendersingh866@gmail.com>
2024-06-12 14:10:36 +0530

    .gitignore file added, folder organised, commenting done also SceneDelegate and AppDelegate

---
+++ CalmoApp/SystemFiles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CalmoApp
+//
+//  Created by Jogender Singh on 12/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // TODO: DEINIT
+    deinit{
+        print("ViewController has been REMOVED...!")
+    }
+    
+    // MARK: - ACTIONS
+}
+
